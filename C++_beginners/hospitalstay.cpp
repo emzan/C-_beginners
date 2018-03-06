a33c56ebf478c38192a2a8481f5300e1da9e0324
Emilio Zanetti <zanettiemilio@hotmail.com>
2018-03-06 15:46:37 +0800

    pushingAfternoon

---
+++ C++_beginners/hospitalstay.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+double calculateCharges(int, double, double, double);
+double calculateCharges(double, double);
+
+int main()
+{
+	int days;
+	double dailyRate, medCharges, labCharges, totalDue;
+	bool inpatient = false;
+	char answer;
+
+	cout << "Is this an impatient stay?" << endl;
+	cin >> answer;
+	while (answer != 'y' && answer != 'n')
+	{
+		cout << "Please enter y/n" << endl;
+		cin >> answer;
+	}
+	if (answer == 'y')
+		inpatient = true;
+	else
+		inpatient = false;
+
+	cout << "Enter the total medication charges: " << endl;
+	cin >> medCharges;
+	cout << "Enter the total lab charges: " << endl;
+	cin >> labCharges;
+	
+	if (inpatient == true)
+	{
+		cout << "please enter days spent in the hospital: " << endl;
+		cin >> days;
+		cout << "please enter daily rate: " << endl;
+		cin >> dailyRate;
+
+		totalDue = calculateCharges(days, dailyRate,
+			medCharges, labCharges);
+	}
+	else
+	{
+		totalDue = calculateCharges(medCharges, labCharges);
+	}
+
+	cout << "The total cost is: $" << totalDue << endl;
+	return 0;
+}
+
+double calculateCharges(int d, double r, double m, double l)
+{
+	return d * r + m + l;
+}
+
+double calculateCharges(double m, double l)
+{
+	return m + l;
+}
